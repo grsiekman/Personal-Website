4cc6879a6985292721ff53a238dca1e6c73617fb
grsiekman <161631826+grsiekman@users.noreply.github.com>
2025-01-27 10:46:42 -0500

    Update touch_hover.js

--- touch_hover.js
+++ touch_hover.js
@@ -1,14 +1,14 @@
-document.addEventListener('touchstart', function(event) {
+document.addEventListener('touchend', function(event) {
   // Get all social media icons
-  var icons = document.getElementsByClassName('contact-icon');
+  var icons = document.getElementsByClassName('social-media-icon');
 
-  // Loop through all icons and add the touch-hover class on touchstart
-    for (var i = 0; i < icons.length; i++) {
-      icons[i].classList.add('touch-hover');
+  // Loop through all icons and add the touch-hover class on touchend
+  for (var i = 0; i < icons.length; i++) {
+    icons[i].classList.add('touch-hover');
 
-      // Remove the touch-hover class after a short delay
-      setTimeout(function() {
-        icons[i].classList.remove('touch-hover');
-      }, 200);
-    }
-  }, false);
+    // Remove the touch-hover class after a short delay
+    setTimeout(function() {
+      icons[i].classList.remove('touch-hover');
+    }, 200);
+  }
+}, false);
