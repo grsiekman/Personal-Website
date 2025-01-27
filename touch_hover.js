bb3f6d9d2118796cc806c7abe5ed5afa91c35a91
grsiekman <161631826+grsiekman@users.noreply.github.com>
2025-01-27 10:44:15 -0500

    Update touch_hover.js

--- touch_hover.js
+++ touch_hover.js
@@ -3,13 +3,12 @@ document.addEventListener('touchstart', function(event) {
   var icons = document.getElementsByClassName('contact-icon');
 
   // Loop through all icons and add the touch-hover class on touchstart
-  for (var i = 0; i < icons.length; i++) {
-    icons[i].classList.add('touch-hover');
-  }
+    for (var i = 0; i < icons.length; i++) {
+      icons[i].classList.add('touch-hover');
 
-  // Remove the touch-hover class after a short delay
-    setTimeout(function() {
-      icons[i].classList.remove('touch-hover');
-    }, 200);
-  }
-}, false);
+      // Remove the touch-hover class after a short delay
+      setTimeout(function() {
+        icons[i].classList.remove('touch-hover');
+      }, 200);
+    }
+  }, false);
