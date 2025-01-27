e2f4df448a346649d9a31bf85a31de7f2f5c6d1d
grsiekman <161631826+grsiekman@users.noreply.github.com>
2025-01-27 11:05:04 -0500

    Update touch_hover.js

--- touch_hover.js
+++ touch_hover.js
@@ -15,3 +15,21 @@ document.addEventListener('click', function(event) {
     }
   }
 }, false);
+
+document.addEventListener('auxclick', function(event) {
+  // Check if the clicked element is a social media icon
+  if (event.target.matches('.content-icon')) {
+    // Get all social media icons
+    var icons = document.getElementsByClassName('content-icon');
+
+    // Loop through all icons and add the touch-hover class on auxclick
+    for (var i = 0; i < icons.length; i++) {
+      icons[i].classList.add('touch-hover');
+
+      // Remove the touch-hover class after a short delay
+      setTimeout(function() {
+        icons[i].classList.remove('touch-hover');
+      }, 200);
+    }
+  }
+}, false);
