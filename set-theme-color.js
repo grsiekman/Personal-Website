dbef04a8d79cf6e7945654427107f8f8ca918e69
grsiekman <161631826+grsiekman@users.noreply.github.com>
2025-01-27 21:16:03 -0500

    v1.6
    theme-color working, variable based on system preferences, changes with switch.

--- set-theme-color.js
+++ set-theme-color.js
@@ -1,11 +1,13 @@
-const metaTheme = document.querySelector('meta[name="theme-color"]');
-
-function setThemeColor() {
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        metaTheme.setAttribute('content', '#273745');
-    } else {
-        metaTheme.setAttribute('content', '#415b74');
-    }
+function setThemeColorBasedOnSystemPreferences() {
+  // Check if the user has a preference for a light or dark color scheme
+  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    // If the user prefers a dark color scheme, set the meta theme-color to a dark color
+    document.querySelector('meta[name="theme-color"]').setAttribute('content', '#273745');
+  } else {
+    // If the user prefers a light color scheme or doesn't have a preference, set the meta theme-color to a light color
+    document.querySelector('meta[name="theme-color"]').setAttribute('content', '#415b74');
+  }
 }
 
-setThemeColor();
+// Call the function to set the meta theme-color based on the user's system preferences
+setThemeColorBasedOnSystemPreferences();
