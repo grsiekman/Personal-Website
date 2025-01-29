a66d735f25e26fc91c2a016f77d1c848e10b2a7e
grsiekman <161631826+grsiekman@users.noreply.github.com>
2025-01-29 12:35:19 -0500

    Update mobile-click.js

--- mobile-click.js
+++ mobile-click.js
@@ -1,12 +1,14 @@
 const images = document.querySelectorAll('#social-menu ul li img');
 
+window.addEventListener('pageshow', () => {
+  images.forEach(image => {
+    image.classList.remove('active');
+  });
+});
+
 images.forEach(image => {
   image.addEventListener('click', () => {
     images.forEach(img => img.classList.remove('active'));
     image.classList.add('active');
-
-    setTimeout(() => {
-      image.classList.remove('active');
-    }, 300);
   });
 });
