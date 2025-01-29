eead4e2e74fa8d9eba1b50d49f80a1faef09e553
grsiekman <161631826+grsiekman@users.noreply.github.com>
2025-01-29 15:04:29 -0500

    Update social-button.js

--- social-button.js
+++ social-button.js
@@ -1,13 +1,13 @@
 const socialButton = document.querySelector('#social-menu ul li img');
 
 socialButton.addEventListener('touchstart', () => {
-	socialButton.style.setAttribute('filter', 'grayscale(0)');
-	socialButton.style.setAttribute('opacity', '100%');
-	socialButton.style.setAttribute('transform', 'scale(1.2)');
+    socialButton.style.filter = 'grayscale(0)';
+    socialButton.style.opacity = '100%';
+    socialButton.style.transform = 'scale(1.2)';
 
-	setTimeout(() => {
-    socialButton.style.setAttribute('filter', 'grayscale(1)');
-    socialButton.style.setAttribute('opacity', '75%');
-    socialButton.style.setAttribute('transform', 'scale(1)');
-}, 500);
+    setTimeout(() => {
+        socialButton.style.filter = 'grayscale(1)';
+        socialButton.style.opacity = '75%';
+        socialButton.style.transform = 'scale(1)';
+    }, 500);
 });
