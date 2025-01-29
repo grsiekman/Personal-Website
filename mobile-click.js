3ce62ee3a3d6966f7afb328296b7d2cccc7eafa2
grsiekman <161631826+grsiekman@users.noreply.github.com>
2025-01-29 12:38:42 -0500

    Update mobile-click.js

--- mobile-click.js
+++ mobile-click.js
@@ -1,15 +1,25 @@
 const images = document.querySelectorAll('#social-menu ul li img');
+let isImageClicked = false;
 
-document.addEventListener('visibilitychange', () => {
-  if (document.visibilityState === 'hidden') {
+window.addEventListener('focus', () => {
+  if (!isImageClicked) {
     images.forEach(image => {
       image.classList.remove('active');
     });
   }
+  isImageClicked = false;
 });
 
 images.forEach(image => {
   image.addEventListener('click', () => {
+    isImageClicked = true;
+    images.forEach(img => img.classList.remove('active'));
+    image.classList.add('active');
+  });
+
+  // Workaround for Safari on iOS
+  image.addEventListener('touchstart', () => {
+    isImageClicked = true;
     images.forEach(img => img.classList.remove('active'));
     image.classList.add('active');
   });
