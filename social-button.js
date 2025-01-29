55580eeaaf1260f8b534e67d30c9a36bf5f28d52
grsiekman <161631826+grsiekman@users.noreply.github.com>
2025-01-29 14:42:06 -0500

    Update social-button.js

--- social-button.js
+++ social-button.js
@@ -2,12 +2,8 @@ const socialButton = document.querySelector('#social-menu ul li img');
 
 socialButton.addEventListener('touchstart', () => {
 	socialButton.classList.add('active');
-});
 
-socialButton.addEventListener('touchend', () => {
-	
-});
-
-setTimeout(() => {
+	setTimeout(() => {
     socialButton.classList.remove('active');
 }, 500);
+});
