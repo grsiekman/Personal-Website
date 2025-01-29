4ecf4cd56c2f92397ad1ccd7bad17a1cb9ea8467
grsiekman <161631826+grsiekman@users.noreply.github.com>
2025-01-28 22:09:36 -0500

    v1.3
    Updated Privacy Policy, fully functional dark theme button and privacy pop-up.

--- data-theme.js
+++ data-theme.js
@@ -1,8 +1,11 @@
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 const metaThemeColor = document.querySelector('meta[name="theme-color"]');
+const dismissButton = document.querySelector('#popup-dismiss-button');
+const DARK_THEME_COLOR = '#273745';
+const LIGHT_THEME_COLOR = '#415b74';
 
-
+//This function sets the website theme based on system preferences
 function setThemeBasedOnSystemPreference() {
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
         document.documentElement.setAttribute('data-theme', 'dark');
@@ -13,6 +16,8 @@ function setThemeBasedOnSystemPreference() {
     }
 }
 
+/*This checks for contents in the currentTheme const, sets data-theme if there is, and calls
+the system preference function if not. Sets switch to on if dark mode is on.*/
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
 
@@ -21,23 +26,53 @@ if (currentTheme) {
     }
  } else {
     setThemeBasedOnSystemPreference();
-
-    if (currentTheme === 'dark') {
-        toggleSwitch.checked = true;
-    }
  }
 
+// This function controls the privacy alert popup, allowing it to pop up only the first time the user clicks the switch
+function privacyPopup() {
+    const clickHistory = localStorage.getItem('clickHist');
+    if (clickHistory === null) {
+    document.querySelector('#popup').style.display = 'unset';
+    try {
+        localStorage.setItem('clickHist', 'clicked');
+    } catch (e) {
+            console.error('LocalStorage is not available:', e);
+        }
+    }
+}
+
+//This function is to switch the theme
 function switchTheme(e) {
+    privacyPopup();
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
-        metaThemeColor.setAttribute('content', '#273745');
+        try {
+            localStorage.setItem('theme', 'dark');
+        } catch (e) {
+            console.error('LocalStorage is not available:', e);
+        }
+        metaThemeColor.setAttribute('content', DARK_THEME_COLOR);
     }
     else {
         document.documentElement.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme', 'light');
-        metaThemeColor.setAttribute('content', '#415b74');
+        try {
+            localStorage.setItem('theme', 'light');
+        } catch (e) {
+            console.error('LocalStorage is not available:', e);
+        }
+        metaThemeColor.setAttribute('content', LIGHT_THEME_COLOR);
     }    
 }
 
-toggleSwitch.addEventListener('change', switchTheme, false);
+//This looks for a switch click and triggers the setTheme function
+if(toggleSwitch) {
+    toggleSwitch.addEventListener('change', switchTheme, false);
+}
+
+function buttonClick(event) {
+        document.querySelector('#popup').style.display = 'none';
+}
+
+if(dismissButton) {
+    dismissButton.addEventListener('click', buttonClick, false);
+}
