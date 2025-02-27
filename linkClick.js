2a4841a92d3867d221a6b4d8153f92be048a920d
grsiekman <161631826+grsiekman@users.noreply.github.com>
2025-02-26 21:39:20 -0500

    v1.6
    Links page fully built out, linkClick.js working as desired, css updated

--- linkClick.js
+++ linkClick.js
@@ -1,7 +1,29 @@
 if (window.matchMedia("(pointer: fine)").matches) {
-     window.addEventListener('pageshow', function(event) {
-            const link = querySelectorAll('links-menu div');
-            hoverElements.forEach(function(element) {
-                element.classList.remove('hover');
-            });
+    const links = document.querySelectorAll('.link-menu-div');
+    const hoverStyle = {
+        transform: 'scale(1.06) translateX(-47.5%)',
+        transition: 'all .2s',
+    };
+    const defaultStyle = {
+        transform: 'scale(1) translateX(-50%)',
+        transition: 'all .2s',
+    };
+    const clickStyle = {
+        transform: 'scale(1) translateX(-50%)',
+        transition: 'all .01s',
+    };
+
+    links.forEach(link => {
+        link.addEventListener("mouseover", () => {
+            Object.assign(link.style, hoverStyle);
         });
+
+        link.addEventListener("mouseout", () => {
+            Object.assign(link.style, defaultStyle);
+        });
+
+        link.addEventListener("mousedown", () => {
+            Object.assign(link.style, clickStyle);
+        });
+    });
+}
