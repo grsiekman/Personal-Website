8f0292c265e7fec714179dc636495244042c4dc2
siekmang <github@siekmang.com>
2025-05-14 17:34:40 -0400

    v2 - HTML Components (#8)
    * Started breaking reused code into HTML components in JavaScript
    
    * HTML components broken out
    
    The reused HTML code blocks are now in their own JavaScript file to be called. Some of these include if statements to make minor tweaks based on the page they are on.
    
    * Update footer.js
    
    * Tweaks to load orders
    
    * Load time performance fixes
    
    * Performance tweaks
    
    * Performance tweaks
    
    ---------
    
    Co-authored-by: grsiekman <161631826+grsiekman@users.noreply.github.com>

---
+++ nametag.js
@@ -0,0 +1,20 @@
+let nametag = document.querySelector('#nametag');
+let header = `<h1>Greg Siekman</h1>`
+
+var path = window.location.pathname;
+var page = path.split("/").pop();
+
+if(page == "privacy.html") {
+    header = `<h1 class="privacy-h1">Privacy Policy</h1>`;
+}
+
+nametag.innerHTML = `
+    ${header}
+    <div class="menu-pill">
+        <ul class="site-nav">
+            <li class="site-nav-item"><a href="index.html" rel="noopener noreferrer">Home</a></li>
+            <li class="site-nav-item"><a href="portfolio.html" rel="noopener noreferrer">Portfolio</a></li>
+            <li class="site-nav-item"><a href="privacy.html" rel="noopener noreferrer">Privacy Policy</a></li>
+        </ul>
+    </div>
+`;
